8d756adfd5a5bee578e2849e7c19d3519948b387
DaywisonSilva <daywisonsilvasou@gmail.com>
2023-05-26 22:36:34 -0300

    fix: adjust side

--- src/components/HomeSection/HomeSection.tsx
+++ src/components/HomeSection/HomeSection.tsx
@@ -18,7 +18,7 @@ const HomeSection = ({ content, image, title }: HomeSectionProps) => {
       />
       <div className='text-white w-full p-5 sm:flex items-center '>
         <div>
-          <h2 className='text-4xl font-bold mb-5 relative after:content-[""] after:block after:w-[40%] after:absolute after:bottom-[-6px] after:h-[1px] after:bg-white'>
+          <h2 className='text-4xl font-bold inline-block mb-5 relative after:content-[""] after:block after:w-[60%] after:absolute after:bottom-[-6px] after:h-[1px] after:bg-white'>
             {title}
           </h2>
           <p>{content}</p>
